940bca8cfd68e9203204461ad6ceb4538bb28b2a
Hedinn Eiriksson <hedinne@gmail.com>
2017-02-14 17:15:07 +0000

    Setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./server/static'));
+app.use(express.static('./client/dist'));
+
+
+app.listen(3000, () => {
+  console.log('🍷 Server is running on http://localhost:3000 .')
+})
