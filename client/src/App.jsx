736a3c2c701da31a7d798aca3dc3cbc5dd8b067a
Hedinn Eiriksson <hedinne@gmail.com>
2017-05-24 14:53:19 +0000

    ups for got this.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 /* eslint no-undef: 0 */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'babel-polyfill';
+import 'es6-promise';
+import 'isomorphic-fetch';
 import Routing from './Routing.jsx';
 
-require('es6-promise').polyfill();
-require('isomorphic-fetch');
 
 ReactDOM.render(<Routing />, document.getElementById('app'));
